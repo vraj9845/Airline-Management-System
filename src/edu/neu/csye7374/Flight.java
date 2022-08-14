c2c25e90478deebe386ed12b0cd86e248ebf638d
Jingru Xiang <xiang.jing@northeastern.edu>
2022-08-14 13:19:02 -0400

    feat: FlightAPI and Flight class

--- src/edu/neu/csye7374/Flight.java
+++ src/edu/neu/csye7374/Flight.java
@@ -1,5 +1,31 @@
 package edu.neu.csye7374;
 
-public class Flight {
+import java.util.Date;
 
+public class Flight implements FlightAPI{
+    private int flightID;
+    private double price;
+    private Date flightDate;
+    private String startSite;
+    private String arriveSite;
+
+    public int getFlightID() {
+        return flightID;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Date getFlightDate() {
+        return flightDate;
+    }
+
+    public String getStartSite() {
+        return startSite;
+    }
+
+    public String getArriveSite() {
+        return arriveSite;
+    }
 }
--- src/edu/neu/csye7374/FlightAPI.java
+++ src/edu/neu/csye7374/FlightAPI.java
@@ -1,5 +1,12 @@
 package edu.neu.csye7374;
 
+import java.util.Date;
+
 public interface FlightAPI {
+    int getFlightID();
+    double getPrice();
+    Date getFlightDate();
+    String getStartSite();
+    String getArriveSite();
 
 }
